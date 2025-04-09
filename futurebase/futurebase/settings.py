4149fefe72b4f1f6d066c9cfb54039a329457a2e
lordace-coder <lordyacey@gmail.com>
2025-04-09 04:14:58 +0200

    settup cors

--- futurebase/futurebase/settings.py
+++ futurebase/futurebase/settings.py
@@ -128,3 +128,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# allow all origins
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
